696c6f0b43f2382282be08d3aeec557e734711e7
shareefain-khan <shareefainkhan.sce@gmail.com>
2024-05-17 18:58:34 +0500

    product file create and picture change function

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ document.getElementById("menu-icon").addEventListener("click", function(event) {
     }
 });
 
+
+// new javascript product gallery
+
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("SmallImg");
+
+SmallImg[0].onclick = function() {
+    ProductImg.src = SmallImg[0].src;
+}
+
+SmallImg[1].onclick = function() {
+    ProductImg.src = SmallImg[1].src;
+}
+
+SmallImg[2].onclick = function() {
+    ProductImg.src = SmallImg[2].src;
+}
+
+SmallImg[3].onclick = function() {
+    ProductImg.src = SmallImg[3].src;
+}
+
+
+
